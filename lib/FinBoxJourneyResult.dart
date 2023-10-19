af5befcc8094d45c5cc7d95305d0c5c42524e689
Srikar Reddy <gsrikar2010@gmail.com>
2023-10-19 19:27:35 +0530

    Except linkId, make rest of the variables nullable

--- lib/FinBoxJourneyResult.dart
+++ lib/FinBoxJourneyResult.dart
@@ -1,16 +1,16 @@
 class FinBoxJourneyResult {
 
-  String entityId;
+  String? entityId;
   String linkId;
-  String message;
-  String errorType;
+  String? message;
+  String? errorType;
 
 
   FinBoxJourneyResult(this.entityId,this.linkId,this.message,this.errorType);
 
-  factory FinBoxJourneyResult.fromJson(dynamic json) {
-    return FinBoxJourneyResult(json['entityId'] as String, json['linkId'] as String,
-        json['message'] as String, json['errorType'] as String );
+  factory FinBoxJourneyResult.fromJson(Map<String, dynamic> json) {
+    return FinBoxJourneyResult(json['entityId'] as String?, json['linkId'] as String,
+        json['message'] as String?, json['errorType'] as String? );
   }
 
   @override
